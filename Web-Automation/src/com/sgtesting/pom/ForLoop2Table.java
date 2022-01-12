4db76bc4a8b98064ad8c8e37ac71da204b12c75b
PremalaKhot <premlakhot108@gmail.com>
2022-01-12 23:57:01 +0530

    Table2

---
+++ Web-Automation/src/com/sgtesting/pom/ForLoop2Table.java
@@ -0,0 +1,13 @@
+package com.sgtesting.pom;
+class ForLoop2Table
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=10;i++)
+		{
+			int res=(i*2);
+			//System.out.println(res);
+			System.out.println("2*"+i+"="+res);
+		}
+	}
+}
